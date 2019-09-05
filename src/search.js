818051c5ddcadbca74853aefdbba9c223f30a610
Kaleo <kaleocheng@gmail.com>
2019-09-05 10:47:28 +0800

    Update search.js

--- src/search.js
+++ src/search.js
@@ -1,24 +1,9 @@
-const Fuse = require('fuse.js')
-const blog = require('/Users/kaleocheng/Github/blog/public/index.json')
-
-const  options = {
-  keys: ['title', 'contents', 'categories', 'tags', 'permalink']
-}
-const fuse = new Fuse(blog, options)
-
+const request = require('request')
 module.exports = (pluginContext) => {
   return (search, env = {}) => {
     return new Promise((resolve, reject) => {
-      fuse.search(search).then(res => {
-      resolve([
-        {
-          icon: 'fa-safari',
-          title: res.title,
-          subtitle: res.contents,
-          value: res.permalink,
-        },
-      ])
-      })
-    });
-  };
-};
+        let url = `http://127.0.0.1:3000/search/${search}`
+        request.get({url:url, json:true}, function (e, r, res) {
+        resolve(res)
+
+    })})}}
